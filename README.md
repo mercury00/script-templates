b3f95ba95b5461870d0897c39b64c97e7ef7f19e
Aaron Thomas <nobody@localhost>
2018-09-11 14:08:56 -0700

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # script-templates
 merely some starter code for writing a simple script
+
+# bash template
+*how to use*
+the bash template has several features and sections for easily writing a quick script that handles errors and does some parsing of command line options and such.
+
+globals: many simple variables can be stored in the $_GLOBALS_ array (dictionary). Constants that you might want to edit from one script to the next are defined in the set_constants function at the top of the script for easy locating/editing during development or when things change.
+
+colors are defined for some useage:
---
+++ python_function_template.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# $Id: script 2 2017-01-01 12:00:00Z user $ (work_name)
+#....................................................................
+# This is a default template to begin writing a simple python script
+# You can add a description of your script in this box
+#....................................................................
+#
+from sys import exit
+
+GLOBALS={}
+def debug(message):
+    """ print out debug messages when the debug flag is on
+    """
+    if GLOBALS['verbose']:
+        print(message)
+
+def parse_opts():
+    """ docstring
+    """
+    from argparse import ArgumentParser, REMAINDER
+
+    parser = ArgumentParser(description='A description of this script ')
+    parser.add_argument('--verbose', '-v', action='store_true', help="be verbose")
+    parser.add_argument('--dry-run', '-n', action='store_true', help="don't actually do anything, just say what would be done")
+    ##add your own args here, or collect them all in 'remainders'
+    parser.add_argument('remainders', nargs=REMAINDER)
+
+    cli_args = parser.parse_args()
+    GLOBALS['verbose'] = cli_args.verbose
+    GLOBALS['dry_run'] = cli_args.dry_run
+    GLOBALS['args'] = cli_args.remainders
+
+ ## V insert your own functions here! V
+
+def main():
+    """ The main function: you can call your functinos from here
+    """
+    pass
+
+if __name__ == "__main__":
+    """ Start our script when called; exit when imported
+    """
+    parse_opts()
+    main()
+else:
+    print("This program does not support importing")
+    exit(-1)
+#__END__#
