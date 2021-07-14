bd940050e2eb4d79dc281c5e53135029dce6279f
Aaron <mercury00@live.com>
2021-07-14 13:59:53 -0700

    Update python_function_template.py
    add license text

--- python_function_template.py
+++ python_function_template.py
@@ -2,6 +2,18 @@
 # -*- coding: utf-8 -*-
 #
 # $Id: script 2 2017-01-01 12:00:00Z user $ (work_name)
+# Copyright (C) 2020 Aaron Thomas
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
 #....................................................................
 # This is a default template to begin writing a simple python script
 # You can add a description of your script in this box
